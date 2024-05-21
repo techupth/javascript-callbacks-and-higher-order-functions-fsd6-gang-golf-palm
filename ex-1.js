4aed495f4ead410bd7d0926c520f646d4e56135a
Krittayapornn <krittayaporn.ww@gmail.com>
2024-05-21 10:03:26 +0700

    fix: solve ex1-2

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,23 @@
 // Exercise #1: For Each Function
 
+const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
+const newEmployeeSalaries =[];
+
+//  for (let i=0; i<employeeSalaries.length; i++ ) {
+//   newEmployeeSalaries.push(employeeSalaries[i]+5000);
+// }
+
+
 function forEach(array, operation) {
   // Start coding here
+  for (let i = 0 ; i < array.length; i++){
+     array[i] = operation(array[i])
+  }
 }
 
-const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
-
 // Using `forEach` function here
+forEach(employeeSalaries, function(salary) {
+  newEmployeeSalaries.push(salary + 5000);
+});
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,41 @@
 //Exercise #2: At Least Five Function
 
-function atLeastFive(array, operation) {
-  // Start coding here
+function atLeastFive(array, operation,) {
+    let count = 0;
+    // Start coding here
+    for (let i = 0; i < array.length; i++) {
+      
+      if (operation(array[i])) {
+        count++;
+      }
+    }
+    if (count >= 5) {
+      return "ผ่าน";
+    } else {
+      return "ไม่ผ่าน";
+    }
+  }
+  
+function check (score){
+  return score > 70;
 }
-
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+// let scoreRoom1Result = [];
+// let scoreRoom2Result = [];
+// let scoreRoom3Result = [];
+
+
+let scoreRoom1Result = atLeastFive(studentScoresRoom1,check)
+  
+  let scoreRoom2Result= atLeastFive(studentScoresRoom2,check)
+  
+  let scoreRoom3Result= atLeastFive(studentScoresRoom3,check)
+  
+  console.log("คุณภาพของนักเรียนในห้อง 1:", scoreRoom1Result);
+  console.log("คุณภาพของนักเรียนในห้อง 2:", scoreRoom2Result);
+  console.log("คุณภาพของนักเรียนในห้อง 3:", scoreRoom3Result);
